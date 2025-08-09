1c7c45f274e4080320a95347ce13038f048babed
bilal <bilal@sgbi.us>
2025-08-10 01:18:09 +0530

    main img updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-// import bgVideo from "../assets/athirapilly_mainimg.jpg";
+import bgimg from "../assets/atirapillyhomeimg.jpg";
 import bgMusic from "../assets/waterfalls.mp3";
 import BookingTabs from "./BookingTab";
 import bgVideo from "../assets/waterfalls2.mp4";
@@ -70,13 +70,19 @@ const Hero = () => {
   
   return (
     <header className="w-screen h-screen relative">
-      {/* <audio ref={audioRef} src={bgMusic} loop preload="auto" /> */}
-      <video
+      <audio ref={audioRef} src={bgMusic} loop preload="auto" />
+      {/* <video
         src={bgVideo}
         className="w-full h-full object-cover"
         autoPlay
         loop
         muted
+      /> */}
+
+            <img
+        src={bgimg}
+        className="w-full h-full object-cover"
+        alt="Athirappilly Waterfalls"
       />
       <div className="absolute top-0 left-0 w-full h-full bg-gray-900/30"></div>
       <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center text-center">
@@ -84,32 +90,7 @@ const Hero = () => {
         <h2 className="text-white mb-4 mt-10">
           Book Your Unforgettable Tour to Kerala’s "Niagara"
         </h2>
-        {/* <form
-          action=""
-          className="flex border p-1 rounded-md text-black bg-gray-100/90 max-w-[700px] w-[80%] mx-auto"
-        >
-          <input
-            type="text"
-            placeholder="Search Destinations"
-            className="grow bg-transparent outline-none"
-          />
-          <button className="w-11 btn--form">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-              />
-            </svg>
-          </button>
-        </form> */}
+
         <BookingTabs/>
       </div>
     </header>
